573931f5e034096b578aea362379b305a8249fe2
Sagar Anand <sagar.anand@mendix.com>
2025-07-27 14:08:05 +0200

    fix: theme addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,104 @@
 
 
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
 
+import AppLayout from "./layout/AppLayout";
+import Home from "./pages/Dashboard/Home";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
+export default function App() {
   return (
-    <main className="container">
-      <h1>Hot Reloading works!</h1>
+    <>
+      <Router>
+        {/* <ScrollToTop /> */}
+        <Routes>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+          {/* Dashboard Layout */}
+          <Route element={<AppLayout />}>
+            <Route index path="/" element={<Home />} />
+            {/* <Route index path="/" element={<Home />} /> */}
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
+            {/* Others Page */}
+            {/* <Route path="/profile" element={<UserProfiles />} /> */}
+            {/* <Route path="/calendar" element={<Calendar />} /> */}
+            {/* <Route path="/blank" element={<Blank />} /> */}
+
+            {/* Forms */}
+            {/* <Route path="/form-elements" element={<FormElements />} /> */}
+
+            {/* Tables */}
+            {/* <Route path="/basic-tables" element={<BasicTables />} /> */}
+
+            {/* Ui Elements */}
+            {/* <Route path="/alerts" element={<Alerts />} /> */}
+            {/* <Route path="/avatars" element={<Avatars />} /> */}
+            {/* <Route path="/badge" element={<Badges />} /> */}
+            {/* <Route path="/buttons" element={<Buttons />} /> */}
+            {/* <Route path="/images" element={<Images />} /> */}
+            {/* <Route path="/videos" element={<Videos />} /> */}
+
+            {/* Charts */}
+            {/* <Route path="/line-chart" element={<LineChart />} /> */}
+            {/* <Route path="/bar-chart" element={<BarChart />} /> */}
+          </Route>
+
+          {/* Auth Layout */}
+          {/* <Route path="/signin" element={<SignIn />} /> */}
+          {/* <Route path="/signup" element={<SignUp />} /> */}
+
+          {/* Fallback Route */}
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </Router>
+    </>
   );
 }
 
-export default App;
+// function App() {
+//   const [greetMsg, setGreetMsg] = useState("");
+//   const [name, setName] = useState("");
+
+//   async function greet() {
+//     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+//     setGreetMsg(await invoke("greet", { name }));
+//   }
+
+//   return (
+//     <main className="container">
+//       <h1>Hot Reloading works!</h1>
+
+//       <div className="row">
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+//         </a>
+//         <a href="https://tauri.app" target="_blank">
+//           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+//         </a>
+//         <a href="https://reactjs.org" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+//       <form
+//         className="row"
+//         onSubmit={(e) => {
+//           e.preventDefault();
+//           greet();
+//         }}
+//       >
+//         <input
+//           id="greet-input"
+//           onChange={(e) => setName(e.currentTarget.value)}
+//           placeholder="Enter a name..."
+//         />
+//         <button type="submit">Greet</button>
+//       </form>
+//       <p>{greetMsg}</p>
+//     </main>
+//   );
+// }
+
+// export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -9,18 +9,40 @@
 // );
 
 
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+// import { StrictMode } from 'react';
+// import * as ReactDOM from 'react-dom/client';
+// import { RouterProvider } from 'react-router-dom';
 
-import  Router  from './router';
+// import  Router  from './router';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
 
-root.render(
+// root.render(
+//   <StrictMode>
+//     <RouterProvider router={Router()} />
+//   </StrictMode>
+// );
+
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "swiper/swiper-bundle.css";
+import "flatpickr/dist/flatpickr.css";
+import App from "./App.tsx";
+
+import { ThemeProvider } from "./context/ThemeContext.tsx";
+
+import { AppWrapper } from "./components/common/PageMeta.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={Router()} />
-  </StrictMode>
+    <ThemeProvider>
+      <AppWrapper>
+        <App />
+      </AppWrapper>
+    </ThemeProvider>
+  </StrictMode>,
 );
